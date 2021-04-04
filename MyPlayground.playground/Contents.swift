59ec30c10de596e10031d5cd9fa454f823dc594e
Rinat Latypov <64914253+neosork@users.noreply.github.com>
2021-04-04 20:48:45 +0300

    gvgvg

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,28 @@
-import UIKit
+func hasAnyMatches (list: [Int], condition: (Int) -> Bool) -> Bool{
+    for item in list {
+        if condition(item){
+            return true
+        }
+    }
+    return false
+}
+func lessThanTen(number: Int) -> Bool {
+    return number < 1
+}
 
-var str = "Hello, playground"
+var numbers  = [20, 19, 7, 12]
+hasAnyMatches(list: numbers, condition: lessThanTen)
+
+numbers.map({ (number: Int) -> Int in
+    var result = 0
+                if number%2 == 0{
+                    result = number
+                } else {
+                    result = 0
+                }
+    return result
+})
+
+for num in numbers {
+    print(num)
+}
